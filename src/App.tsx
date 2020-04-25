9b691115bc620d1272716046a35e9b7799b42fb9
Jade Hayes <Jade.e.Hayes@gmail.com>
2020-04-24 22:49:17 -0700

    * Functional example.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [visible, toggleVisible] = useState(false)
+  const [color, updateColor] = useState("blue")
+
+  useEffect(() => { console.log("Logo state was updated.") }, [visible])
+
+  const randomNum = () => Math.floor(Math.random() * 256)
+  const buttonClick = () => { toggleVisible(!visible) }
+  const changeStuff = () => {
+    let r = randomNum()
+    let g = randomNum()
+    let b = randomNum()
+    updateColor(`rgb(${r}, ${g}, ${b})`)
+  }
+
   return (
-    <div className="App">
+    <>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div hidden={visible} onClick={changeStuff}>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hey Tommy! <code>:D</code>
+          </p>
+        </div>
+        <h1 style={{ color: color }} >{visible ? "visible" : "hidden"}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,8 +36,9 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={buttonClick}>Click me please</button>
       </header>
-    </div>
+    </>
   );
 }
 
